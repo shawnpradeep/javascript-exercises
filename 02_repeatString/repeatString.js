d8b3d2050206482fa6a64bd49297f92747533ed0
Shawn Pradeep <93854780+shawnpradeep@users.noreply.github.com>
2023-03-11 12:57:16 -0500

    finished assignment 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+  if (num < 0) return "ERROR";
+  let rt = "";
+  for (let i = 0; i < num; i++) {
+    rt += str;
+  }
 
+  return rt;
 };
 
 // Do not edit below this line
