82e5874da99fb67964738c55296662c06dab0ba4
Shawn Pradeep <shawnprdp@gmail.com>
2023-03-11 23:55:50 -0500

    finished assignment 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+  for (let arg of args) {
+    let iToDelete = arr.indexOf(arg);
+    while (iToDelete != -1) {
+      arr.splice(iToDelete, 1);
+      iToDelete = arr.indexOf(arg);
+    } // while
+  } // for-each
 
+  return arr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
