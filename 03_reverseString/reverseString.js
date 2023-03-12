41c69dfbd78ab65725e5609d10180cc382580483
Shawn Pradeep <shawnprdp@gmail.com>
2023-03-11 20:08:54 -0500

    Finished assignment 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let reverse = "";
+  for (let i = str.length-1; i >= 0; i--) {
+    reverse += str[i];
+  } // for
 
+  return reverse;
 };
 
 // Do not edit below this line
