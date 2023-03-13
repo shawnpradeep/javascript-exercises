c96df3646e30b37fe46057fe7e74f6dc7054b409
Shawn Pradeep <shawnprad@hotmail.com>
2023-03-13 14:56:08 -0400

    finished assigment 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 100 == 0)
+        if (year % 400 != 0) return false;
+    if (year % 4 != 0) return false;
 
+    return true;
 };
 
 // Do not edit below this line
